21a2427cfc4b0640a2f8e0628ab9c9bba9b17e0d
mstn <mstn@git.com>
2015-11-15 13:00:08 +0100

    import firebase lib using bower

---
+++ firebase_core_client.js
@@ -0,0 +1,6 @@
+"use strict"
+const FirebaseRef = Firebase;
+
+Firebase = {};
+// useful for debug
+Firebase._FirebaseRef = FirebaseRef;
---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  summary:'meteor wrapper for firebase',
+  version:'0.0.1',
+  name:'mstn:firebase-core'
+});
+
+Package.onUse(function(api){
+  api.versionsFrom('1.2');
+  api.use('mquandalle:bower@1.5.2_1', 'client');
+  api.addFiles([
+    'bower.json',
+    'firebase_core_client.js'
+  ], 'client');
+
+  // XXX here we should export Firebase
+  // but it is already a global var imported by bower
+  // even if it is overridded in client code
+  // it doesn't like api.export
+
+});
